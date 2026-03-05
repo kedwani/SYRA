60f0943ee852f3583bfde36d2666bef83ede03e0
kedwani <you@example.com>
2026-03-05 22:29:39 +0200

    update admin.cpython-313.pyc, admin.py and admin.py

--- Profiles/admin.py
+++ Profiles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import MedicalProfile
 
-# Register your models here.
+
+@admin.register(MedicalProfile)
+class MedicalProfileAdmin(admin.ModelAdmin):
+    # الحقول اللي هتظهر في القائمة الرئيسية للجدول
+    list_display = ["user", "blood_type", "gender", "has_insurance"]
+    # حقول البحث (عشان تبحث باسم المستخدم)
+    search_fields = ["user__username", "blood_type"]
+    # فلاتر جانبية
+    list_filter = ["blood_type", "gender", "has_insurance"]
