ab2504a5577e6773c3325f53418bef714388fb04
SYRA Developer <developer@syra.health>
2026-03-10 22:29:31 +0000

    Revert ratelimit middleware - requires package installation

--- Syra/settings.py
+++ Syra/settings.py
@@ -33,7 +33,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'ratelimit.middleware.RateLimitMiddleware',
 ]
 
 ROOT_URLCONF = 'syra.urls'
